f72bd3ac155c2a7e1865775ca9eb81473fdad56e
Ilya Meerovich <ilya.meerov@gmail.com>
2023-07-28 14:41:25 -0700

    chore: add linter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/strict'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: ['dist', '.eslintrc.js'],
+};

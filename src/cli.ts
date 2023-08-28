eb23c035715564ebed7c39c2400cea66ee7ffbf4
Ilya Meerovich <ilya.meerov@gmail.com>
2023-08-27 22:20:00 -0700

    feat: add glob support

--- src/cli.ts
+++ src/cli.ts
@@ -1,9 +1,16 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import minimist from 'minimist'
+import minimist, { ParsedArgs } from 'minimist'
+import { globSync } from 'glob'
 import { cloak } from '.'
 
-type Args = { [key: string]: string | boolean }
+interface Args {
+  shouldDisplayHelp: boolean
+  shouldDisplayVersion: boolean
+  shouldDisplayKeys: boolean
+  shouldReplaceFile: boolean
+  quiet: boolean
+}
 
 const supportedOptions = [
   'f',
@@ -12,6 +19,10 @@ const supportedOptions = [
   'in-place',
   'k',
   'keys',
+  'p',
+  'pattern',
+  'q',
+  'quiet',
   'v',
   'version',
   'h',
@@ -25,9 +36,11 @@ Usage:
   json-cloak [options]
 
 Options:
-  -f, --file        Specify JSON file to transform
-  -i, --in-place    Update file in place with transformed JSON object
+  -f, --file        Specify JSON file to transform. Passing multiple files will perform updates **in place**!
+  -i, --in-place    Update file in place with transformed JSON object.
   -k, --keys        Display replaced keys instead of printing transformed JSON object
+  -p, --pattern     Specify a glob pattern to operate on multiple files. This will perform updates **in place**!
+  -q, --quiet       Don't print to stdout
   -v, --version     Display the version number
   -h, --help        Show this help
 
@@ -38,45 +51,52 @@ Options:
  * @returns {Promise<void>}
  */
 export async function main(): Promise<void> {
-  const args = parseArguments()
+  const rawArgs = parseArguments()
+
+  const args = validateArguments(rawArgs)
 
   // Display usage
-  if (Object.keys(args).length === 1 || args.h || args.help) {
+  if (args.shouldDisplayHelp) {
     process.stdout.write(usage)
     return
   }
 
   // Display version
-  if (args.v || args.version) {
+  if (args.shouldDisplayVersion) {
     const version = await getVersion()
     process.stdout.write(version)
     return
   }
 
-  const shouldDisplayKeys = args.k || args.keys
-  const shouldReplaceFile = args.i || args['in-place']
-  const file = args.f || args.file
+  const files = resolveFileList(rawArgs)
 
-  if (typeof file !== 'string') {
-    throw new Error('The `-f, --file` option was either not used or no file argument was passed in.')
+  if (files.length ===  1) {
+    void transformJSON({
+      file: files[0],
+      shouldReplaceFile: args.shouldReplaceFile,
+      shouldDisplayKeys: args.shouldDisplayKeys,
+      quiet: args.quiet
+    })
+
+    return
   }
 
-  if (shouldDisplayKeys && shouldReplaceFile) {
-    throw new Error('The provided options will overwrite file with replaced keys. Aborting operation.')
+  for (const file of files) {
+    process.stdout.write(`Transforming file ${file}...\n`)
+    void transformJSON({
+      file,
+      shouldReplaceFile: true,
+      shouldDisplayKeys: false,
+      quiet: true
+    })
   }
-
-  void transformJSON({
-    file,
-    shouldReplaceFile: shouldReplaceFile as boolean,
-    shouldDisplayKeys: shouldDisplayKeys as boolean
-  })
 }
 
 /**
  * Parses command-line arguments using `minimist`.
  * @returns {Args} The parsed command-line arguments.
  */
-export function parseArguments(): Args {
+export function parseArguments(): ParsedArgs {
   const args = minimist(process.argv.slice(2))
 
   // Check for unsupported options
@@ -88,6 +108,83 @@ export function parseArguments(): Args {
   }
 
   return args
+}
+
+/**
+ * Validates that there are no incompatible argument combinations.
+ * @returns {Args} The validated arguments.
+ */
+export function validateArguments(parsedArgs: ParsedArgs): Args {
+  const args: Args = {
+    shouldDisplayHelp    : Boolean(Object.keys(parsedArgs).length === 1 || parsedArgs.h || parsedArgs.help),
+    shouldDisplayVersion : Boolean(parsedArgs.v || parsedArgs.version),
+    shouldDisplayKeys    : Boolean(parsedArgs.k || parsedArgs.keys),
+    shouldReplaceFile    : Boolean(parsedArgs.i || parsedArgs['in-place']),
+    quiet                : Boolean(parsedArgs.q || parsedArgs.quiet)
+  }
+
+  if (args.shouldDisplayHelp || args.shouldDisplayVersion) {
+    return args
+  }
+
+  if (args.shouldDisplayKeys) {
+    // -k, --keys is incompatible with updating file in place
+    if (args.shouldReplaceFile) {
+      throw new Error('The provided options will overwrite file with replaced keys. Aborting operation.')
+    }
+
+    // -k, --keys is incompatible with operating on multiple files
+    const globPattern = Boolean(parsedArgs.p || parsedArgs.pattern)
+    if (globPattern) {
+      throw new Error('Glob pattern option is incompatible with key display option. Aborting operation.')
+    }
+
+    // -k, --keys is incompatible with operating on multiple files
+    const file = (parsedArgs.f || parsedArgs.file) as string | string[]
+    if (Array.isArray(file)) {
+      throw new Error('Keys can only be displayed for a single file at a time')
+    }
+  }
+
+  return args
+}
+
+/**
+ * Valid files and glob patterns can either be strings or arrays of strings.
+ * For `cloak` to work, at least one of `-f` or `-p` should be a valid type.
+ */
+const isValidFileReference = (arg: unknown): boolean => typeof arg === 'string' || (Array.isArray(arg) && arg.every(item => typeof item === 'string'))
+
+/**
+ * Get list of all files to transform from either one or more individual file arguments,
+ * one or more glob patterns, or both
+ *
+ * @param {Object} rawArgs - Object containing arguments passed to the script, gently massaged by minimist
+ */ 
+export function resolveFileList(rawArgs: ParsedArgs): string[] {
+  const files: unknown = rawArgs.f || rawArgs.file
+  const globs: unknown = rawArgs.p || rawArgs.pattern
+
+  let fileList: string[] = []
+
+  if (!isValidFileReference(files) && !isValidFileReference(globs)) {
+    throw new Error('At least one of `-f, --file` or `-p, --pattern` must be provided.')
+  }
+
+  if (isValidFileReference(files)) {
+    // get individual file args
+    fileList = fileList.concat(files as string | string[])
+  }
+
+  if (isValidFileReference(globs)) {
+    // get files from glob patterns
+    for (const pattern of ([] as string[]).concat(globs as string | string[])) {
+      const resolvedFiles: string[] = globSync(pattern, { cwd: process.cwd(), ignore: 'node_modules/**' }) // eslint-disable-line
+      fileList.push(...resolvedFiles)
+    }
+  }
+
+  return Array.from(new Set(fileList))
 }
 
 /**
@@ -110,25 +207,37 @@ export async function getVersion(): Promise<string> {
  * @param {string} args.file - The path to the JSON file to transform.
  * @param {boolean} args.shouldReplaceFile - Indicates if the file should be updated in place.
  * @param {boolean} args.shouldDisplayKeys - Indicates if only the replaced keys should be displayed.
+ * @param {boolean} args.quiet - If present, don't print to stdout.
  * @returns {Promise<void>} A Promise that resolves when the transformation is complete.
  */
 export async function transformJSON({
   file,
   shouldReplaceFile,
   shouldDisplayKeys,
+  quiet,
 }: {
   file: string
   shouldReplaceFile: boolean
   shouldDisplayKeys: boolean
+  quiet: boolean
 }): Promise<void> {
-  const content = await getFileContents(file)
-  const transformedContent = JSON.stringify(cloak(content, { keys: shouldDisplayKeys }), null, 2)
+  try {
+    const content = await getFileContents(file)
+    const transformedContent = JSON.stringify(cloak(content, { keys: shouldDisplayKeys }), null, 2)
 
-  if (shouldReplaceFile) {
-    void fs.promises.writeFile(path.resolve(process.cwd(), file), transformedContent)
+    if (shouldReplaceFile) {
+      void fs.promises.writeFile(path.resolve(process.cwd(), file), transformedContent)
+    }
+
+    if (quiet) {
+      return
+    }
+
+    process.stdout.write(transformedContent)
+  } catch (err: unknown) {
+    const errorMessage = `Error retrieving package version: ${(err as Error).message}`
+    throw new Error(errorMessage)
   }
-
-  process.stdout.write(transformedContent)
 }
 
 /**
